6c0fee7d3fd1adb0cf10dcf8b63ee06526f39ec8
Khondoker Khademul Bari <barii.py@gmail.com>
2022-01-08 14:41:40 +0600

    [added] ex. path parameter and ordering and type validating

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ app = FastAPI()
 def index():
     json_message = {'message': 'Hello world !!'}
     return json_message
+
+
+# *** Ordering ***
+@app.get('/blog/all')
+def get_all_blog():
+    return {'message':'Provided all blog !!'}
+
+# Example of path parameter
+@app.get('/blog/{id}')
+def get_blog_post(id:int): # pydentic : type validation
+    return {'message': f'Blog with id {id}'}
+
+
