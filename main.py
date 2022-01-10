d9e8f8cb5e46bf5067afa8a1200857770451705c
Khondoker Khademul Bari <barii.py@gmail.com>
2022-01-10 13:03:04 +0600

    [Added] - Another router example of Post

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
-from routers import get_blogs
+from routers import (
+    get_blogs,
+    post_blogs
+)
+
 app = FastAPI()
 
 app.include_router(get_blogs.router)
+app.include_router(post_blogs.router)
