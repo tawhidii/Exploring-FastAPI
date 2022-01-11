6871cacffc621266a88f2e4d914deedc3ca908ce
Khondoker Khademul Bari <barii.py@gmail.com>
2022-01-11 14:33:40 +0600

    [Added] Ex. of request body

--- routers/post_blogs.py
+++ routers/post_blogs.py
@@ -1,9 +1,21 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from typing import Optional
 
 router = APIRouter(prefix='/blog', tags=['blog'])
 
 
+# Example of request body with pydantic BaseModel
+class BlogModel(BaseModel):
+    title: str
+    content: str
+    comment_id:int
+    is_published: Optional[bool]
+
+
 @router.post('/new')
-def create_blog():
+def create_blog(blog: BlogModel):
     """ Endpoint for create blog post."""
-    pass
+    # will convert to automatic JSON
+    print(blog.title)
+    return {'data': blog}
