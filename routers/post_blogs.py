25a29e1766191a04a45fd8572eda4b3176848f67
Khondoker Khademul Bari <barii.py@gmail.com>
2022-01-11 18:45:41 +0600

    [Added] - Ex. of custom subtype

--- routers/post_blogs.py
+++ routers/post_blogs.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, Query, Body
+from fastapi import APIRouter, Query, Body, Path
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List, Dict
 
 router = APIRouter(prefix='/blog', tags=['blog'])
 
@@ -27,23 +27,36 @@ def create_blog(blog: BlogModel, id: int, version: int = 2):  # with path and qu
     }
 
 
+# Custom type
+class Image(BaseModel):
+    url: str
+
+
 class CommentModel(BaseModel):
     title: str
     comment_body: str
+    is_published: Optional[bool]
+    tags: List[str] = []
+    metadata: Dict[str, str] = {'key1': 'value2'}
+    image: Optional[Image] = None
 
 
-# Example of Parameter meta data with validators
+# Example of Parameter metadata with validators
 @router.post('/new/{id}/comment')
 def create_comment(comment: CommentModel,
                    id: int,
-                   comment_id: int = Query(None,
-                                           alias='commentId',
-                                           deprecated=True),
-                   content=Body(...,min_length=10,max_length=100)
+                   comment_title: int = Query(None,
+                                              alias='commentId',
+                                              deprecated=True),
+                   comment_id: int = Path(None, ge=3, le=5),
+                   content=Body(...),
+                   versions: Optional[List[str]] = Query(['1.0', '1.1'])
                    ):
     return {
         'id': id,
         'comment': comment,
         'comment_id': comment_id,
-        'content_id': content
+        'content_id': content,
+        'versions': versions,
+        'comment_title': comment_title
     }
